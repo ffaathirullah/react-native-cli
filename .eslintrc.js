3dc1b09f8a81ecdb503f0f8439c650fa2797e59a
Fachrul <ffaathirullah1@gmail.com>
2020-08-25 14:44:18 +0700

    import.export

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,22 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es2020: true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: [
     'react',
   ],
   rules: {
-    'import/no-named-as-default': 0,
-    'import/no-named-as-default-member': 0,
-    'parser': 'babel-eslint',
   },
 };
